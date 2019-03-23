fe7c91537fb0d9128c2451230a6d673f5ac2e173
gur5381 <21500670@handong.edu>
2019-03-23 15:27:33 +0900

    yes

--- Hash/위장/solution.cpp
+++ Hash/위장/solution.cpp
@@ -8,39 +8,17 @@ using namespace std;
 
 int solution(vector<vector<string>> clothes) {
     //맵을 만들어
-    int sum = 0;
+    int sum = 1;
     map<string, int> m;
-    vector<int> num;
-    map<int, int> count;
 
     for(int i = 0; i < clothes.size(); i++){
         int value = m.find(clothes[i][1])->second;
         if(value == m.end()->second) m.insert(make_pair(clothes[i][1], 1));
         else m[clothes[i][1]] = value + 1;
     }
-    for(auto i = m.begin(); i !=  m.end(); i++)num.push_back(i->second);
-    for(int i = 0; i < num.size(); i++){
-        count.insert(make_pair(num[i], i));
-        sum += num[i];
+    for(auto i = m.begin(); i !=  m.end(); i++){
+        sum *= (i->second +1);
     }
 
-    
-    
-    for(int i = 0; i < num.size(); i++){
-        map<int, int> temp;
-        
-        for(auto j = count.begin(); j !=  count.end(); j++){
-            for(int k = j->second + 1; k < num.size(); k++){
-                int value = j->first*num[k];
-                temp.insert(make_pair(value, k));
-                sum += value;
-            }
-        }
-        count.clear();
-        for(auto j = temp.begin(); j !=  temp.end(); j++){
-            count.insert(make_pair(j->first, j->second));
-        }
-    }
-   
-    return sum;
+    return sum-1;
 }
