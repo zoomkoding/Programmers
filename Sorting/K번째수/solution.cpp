f868318b9c5c6ab8dc052f73d0b3158a8219c668
gur5381 <21500670@handong.edu>
2019-03-26 11:42:40 +0900

    solved first

--- Sorting/K번째수/solution.cpp
+++ Sorting/K번째수/solution.cpp
@@ -1,21 +1,19 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 vector<int> solution(vector<int> array, vector<vector<int>> commands) {
     vector<int> answer;
-    for(int i = 0; i< commands.size(); i++){
-        int min= 101;
-        int bound = 0;
-        for(int j = 0; j < commands[i][2]; j++){
-            min = 101;
-            for(int k = commands[i][0]-1; k <= commands[i][1]-1; k++){
-                if(array[k]<min && array[k]>bound)min = array[k];
-            }
-            bound = min;
-        }
-        answer.push_back(min);
+    for(int i = 0; i < commands.size(); i++){
+        vector<int> copy = array;
+        int start = commands[i][0]-1;
+        int end = commands[i][1];
+        int k = commands[i][2];
+        
+        sort(copy.begin()+start, copy.begin()+end);
+        answer.push_back(copy[start+k-1]);
     }
     return answer;
 }
